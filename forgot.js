7cdc0a79d8a771ba98669bc5f7e7d618b28db3c3
Mireziz-Memmedov <azzmmmdv@gmail.com>
2025-12-29 16:17:34 +0000

    forgot.js

--- forgot.js
+++ forgot.js
@@ -5,4 +5,50 @@ $(document).ready(function () {
         $('body').toggleClass('dark-mode');
     });
 
+    function forgot_check() {
+        const username_or_email = $('#username_or_email').val().trim();
+        const errorMsg = $('#error-msg');
+
+        if (!username_or_email) {
+            errorMsg.html("Zəhmət olmasa istifadəçi<br>adını və ya email daxil edin");
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "https://login-db-backend-three.vercel.app/api/forgot-check/",
+            contentType: "application/json",
+            data: JSON.stringify({
+                username_or_email: username_or_email
+            }),
+            success: function (response) {
+
+                if (response.success) {
+                    window.location.href = "./index.html";
+                } else {
+                    errorMsg.css("color", "red");
+                    errorMsg.html("İstifadəçi adı və ya<br>email yanlışdır!");
+                    $('#username_or_email').val('');
+                }
+            },
+            error: function () {
+                errorMsg.css("color", "red");
+                errorMsg.html("Server xətası,<br>sonra yenidən cəhd edin!");
+                $('#username_or_email').val('');
+            }
+        });
+    }
+
+    $('#login-btn').click(function (e) {
+        e.preventDefault();
+        forgot_check();
+    });
+
+    $(document).on('keypress', function (e) {
+        if (e.which === 13) {
+            e.preventDefault();
+            forgot_check();
+        }
+    });
+
 });
