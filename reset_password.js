99f4faa094e36e8113d9f5b40e8d3d83885678cb
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-01-05 18:23:35 +0000

    reset_password.js

--- reset_password.js
+++ reset_password.js
@@ -17,4 +17,48 @@ $(document).ready(function () {
         }
     });
 
+    function reset_password() {
+
+        const password = $('#password').val().trim();
+        const errorMsg = $('#error-msg');
+
+        if (!password) {
+            errorMsg.html('Yeni şifrə təyin edin!');
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "https://login-db-backend-three.vercel.app/api/reset-password/",
+            contentType: "application/json",
+            data: JSON.stringify({
+                password: password
+            }),
+            success: function (response) {
+                if (response.success) {
+                    window.location.href = "./profil.html";
+                } else {
+                    errorMsg.css("color", "red");
+                    errorMsg.html("Kod yanlışdır!");
+                    $('#password').val('');
+                }
+            },
+            error: function () {
+                errorMsg.css("color", "red");
+                errorMsg.html("Server xətası,<br>sonra yenidən cəhd edin!");
+                $('#password').val('');
+            }
+        });
+    }
+    $('#login-btn').click(function (e) {
+        e.preventDefault();
+        reset_password();
+    });
+
+    $(document).on('keypress', function (e) {
+        if (e.which === 13) {
+            e.preventDefault();
+            reset_password();
+        }
+    });
 });
