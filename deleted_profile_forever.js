d07ea10153c161566e5286e077fc52e3c79d27f7
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-02-07 15:42:40 +0000

    changed

--- deleted_profile_forever.js
+++ deleted_profile_forever.js
@@ -26,4 +26,47 @@ $(document).ready(function () {
             icon.removeClass('fa-eye').addClass('fa-eye-slash');
         }
     });
+
+    function del_forever() {
+
+        const username = localStorage.getItem('currentUsername');
+        const password = $('#password').val().trim();
+        const errorMsg = $('#error-msg');
+
+        if (!username || !password) {
+            errorMsg.css('color', 'red');
+            errorMsg.html('İstifadəçi adı və şifrə mütləqdir!');
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "https://login-db-backend-three.vercel.app/api/delete-profile-forever/",
+            contentType: "application/json",
+            data: JSON.stringify({
+                currentUsername: username,
+                password: password
+            }),
+            success: function (response) {
+                if (response.success) {
+                    alert(response.message);
+                    localStorage.clear();
+                    window.location.href = "./index.html";
+                } else {
+                    errorMsg.css('color', 'red');
+                    errorMsg.html(response.error || 'Şifrə yanlışdır!');
+                    $('#password').val('');
+                    return;
+                }
+            },
+            error: function (xhr, status, error) {
+                console.error("Server error:", error);
+            }
+        });
+    }
+
+    $("#login-btn").on("click", function () {
+        del_forever();
+    });
+
 });
