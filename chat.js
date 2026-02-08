7f35688c9ccf7e92221b99cc74c04e92cd936452
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-02-08 16:35:05 +0000

    changed

--- chat.js
+++ chat.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     }
 
     let offset = 0;
-    const limit = 10;
+    const limit = 19;
     let lastMessageId = 0;
 
     // Mesaj əlavə etmək funksiyası (append = aşağı, prepend = yuxarı)
