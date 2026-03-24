17a13471bae1008928c81acb191a09108d3e7405
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-03-24 07:31:25 +0000

    js

--- translate.js
+++ translate.js
@@ -1,6 +1,33 @@
 $(document).ready(function () {
 
-    let language = localStorage.getItem('translate') || 'AZ'
+    function getTranslation() {
+
+        let language = localStorage.getItem('translate') || 'AZ'
+        const key = $('#login-btn').data('key');
+        // console.log("key:", $('#login-btn').data('key'));
+        // console.log("lang:", localStorage.getItem('translate'));
+        // console.log($('#login-btn').length);
+
+        $.ajax({
+            type: "POST",
+            url: "https://login-db-backend-three.vercel.app/api/translations/",
+            contentType: "application/json",
+            data: JSON.stringify({
+                lang: language,
+                key: key
+            }),
+            success: function (response) {
+                if (response.success) {
+                    // console.log("success:", response);
+                    $('#login-btn').text(response.value);
+                }
+            },
+            error: function (xhr, status, error) {
+                console.error("Server error:", error);
+            }
+        });
+    }
+    getTranslation();
 
     $('#lang-menu li').click(function (e) {
 
@@ -8,6 +35,7 @@ $(document).ready(function () {
         alert('Seçilən dil: ' + lang);
 
         localStorage.setItem('translate', lang)
+        getTranslation();
 
     });
 
